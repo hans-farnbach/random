6755e1d2a081eccb675ec2fdeb79def9b8463418
Hans Farnbach <hans@farnbach.net>
2018-04-03 15:38:16 -0600

    Trimming and a bug fix.

--- dna_reverse_complement.py
+++ dna_reverse_complement.py
@@ -2,20 +2,17 @@
 # Lines in the file must be in the following format:
 #	>Sequence[0-9]+
 #	[ATCG]+
-from re import sub,compile
-
-sequence_pattern = compile(r"(>Sequence\d+)\n([ATCG]+)\b")
+from re import sub, compile
+sequence_pattern = compile(r"(>Sequence\d+)\n([ATCG]+)")
 read_file = input("Input File: ")
 
-with open(read_file) as f:
+with open(read_file) as f:	# input
 	sequence = f.read()
-match = sequence_pattern.search(sequence) # prime the first match
-while match:
+while sequence_pattern.search(sequence):
+	match = sequence_pattern.search(sequence)
 	dna = match[2]
 	dna = "A".join([sub(r"A", r"T", base) for base in dna.split("T")])  # split on T's, flip A's in the resulting list to T's, join back with A's
 	dna = "C".join([sub(r"C", r"G", base) for base in dna.split("G")])  # same as above, but with G's and C's
-	sequence = sub(r"({0})\n[ATCG]+\b".format(match[1]), r"\1_ReverseComplement\n" + dna[::-1], sequence) # rewrite the string again
-	match = sequence_pattern.search(sequence)
-	
-with open("write_file.txt", "w+") as f:
+	sequence = sub(r"({0})\n[ATCG]+".format(match[1]), r"\1_ReverseComplement\n{0}\n".format(dna[::-1]), sequence) # replace the sequence with the reversed one
+with open(read_file + "_out.txt", "w+") as f:	# output
 	f.write(sequence)
